7266099b28addeb9b6970ab8ab63163c2b54bfb3
qaje <javierpal3232@gmail.com>
2024-06-10 22:58:24 -0400

    add views users and authentication'

---
+++ resources/views/saludo.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+</head>
+
+<body>
+    <!-- Your content goes here -->
+    <h2>Saludos mediante un controller</h2>
+
+    {{-- <table class="table table-bordered">
+        @foreach ($users as $user)
+            <tr>
+                {{ $user->id }}
+                <br>
+                {{ $user->name }}
+                <br>
+                {{ $user->email }}
+
+            </tr>
+        @endforeach
+    </table> --}}
+
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('', function () {
+//     return view('welcome');
+// });
+Route::get('users', [UserController::class, 'index'])->name('users');
+
+
+Route::get('saludo',[HomeController::class, 'saludo']);
+
+Route::get('inicio',[HomeController::class, 'inicio']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
